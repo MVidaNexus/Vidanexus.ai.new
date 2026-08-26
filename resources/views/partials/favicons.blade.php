c0790458ba9845c0b373640d0e17011b71985047
Mo Ibrahem <mo-ibrahem-vida@users.noreply.github.com>
2026-08-26 03:35:50 +0300

    fix(favicon): update official AI planet emblem favicon across all resolutions and layouts with cache-busting

--- resources/views/partials/favicons.blade.php
+++ resources/views/partials/favicons.blade.php
@@ -1,8 +1,14 @@
 <!-- VidaNexus Official AI Emblem Favicons -->
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png?v=' . filemtime(public_path('favicon-32x32.png'))) }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png?v=' . filemtime(public_path('favicon-16x16.png'))) }}">
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png?v=' . filemtime(public_path('apple-touch-icon.png'))) }}">
-<link rel="shortcut icon" href="{{ asset('favicon.ico?v=' . filemtime(public_path('favicon.ico'))) }}">
+<link rel="icon" type="image/png" sizes="512x512" href="{{ asset('favicon-512x512.png?v=' . (file_exists(public_path('favicon-512x512.png')) ? filemtime(public_path('favicon-512x512.png')) : '2026_fav')) }}">
+<link rel="icon" type="image/png" sizes="192x192" href="{{ asset('favicon-192x192.png?v=' . (file_exists(public_path('favicon-192x192.png')) ? filemtime(public_path('favicon-192x192.png')) : '2026_fav')) }}">
+<link rel="icon" type="image/png" sizes="64x64" href="{{ asset('favicon-64x64.png?v=' . (file_exists(public_path('favicon-64x64.png')) ? filemtime(public_path('favicon-64x64.png')) : '2026_fav')) }}">
+<link rel="icon" type="image/png" sizes="48x48" href="{{ asset('favicon-48x48.png?v=' . (file_exists(public_path('favicon-48x48.png')) ? filemtime(public_path('favicon-48x48.png')) : '2026_fav')) }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png?v=' . (file_exists(public_path('favicon-32x32.png')) ? filemtime(public_path('favicon-32x32.png')) : '2026_fav')) }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png?v=' . (file_exists(public_path('favicon-16x16.png')) ? filemtime(public_path('favicon-16x16.png')) : '2026_fav')) }}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png?v=' . (file_exists(public_path('apple-touch-icon.png')) ? filemtime(public_path('apple-touch-icon.png')) : '2026_fav')) }}">
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico?v=' . (file_exists(public_path('favicon.ico')) ? filemtime(public_path('favicon.ico')) : '2026_fav')) }}">
+<link rel="icon" type="image/png" href="{{ asset('favicon.png?v=' . (file_exists(public_path('favicon.png')) ? filemtime(public_path('favicon.png')) : '2026_fav')) }}">
 <link rel="manifest" href="{{ asset('site.webmanifest') }}">
 <meta name="theme-color" content="#030712">
 <meta name="msapplication-TileColor" content="#030712">
+<meta name="msapplication-TileImage" content="{{ asset('favicon-512x512.png?v=2026_fav') }}">
